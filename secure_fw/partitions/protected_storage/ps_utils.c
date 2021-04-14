c3d2287f498f12350da87f0eb61123d4366bcb6b
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2021-04-14 14:56:39 +0200

    [nrf fromlist] protected_storage: Enable non static key labels
    -Adds non static key labels for the key derivation
    -Client id + UID is used as the label
    -The ps_table still has a static label
    
    Ref: NCSDK-9006
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    (cherry picked from commit ab7d1848d326c7150cbb9d1006517ecfee51ecba)

--- secure_fw/partitions/protected_storage/ps_utils.c
+++ secure_fw/partitions/protected_storage/ps_utils.c
@@ -27,3 +27,26 @@ psa_status_t ps_utils_check_contained_in(uint32_t superset_size,
 
     return PSA_SUCCESS;
 }
+
+#ifdef PS_ENCRYPTION
+psa_status_t ps_utils_encode_key_label(psa_storage_uid_t uid,
+                                       int32_t client_id,
+                                       uint8_t *buff,
+                                       size_t buff_len)
+{
+
+    if (buff_len < (sizeof(client_id) + sizeof(uid))) {
+        return PSA_ERROR_BUFFER_TOO_SMALL;
+    }
+
+    if (buff == NULL) {
+        return PSA_ERROR_INVALID_ARGUMENT;
+    }
+
+    tfm_memset(buff, 0x0, buff_len);
+    tfm_memcpy(buff, &client_id, sizeof(client_id));
+    tfm_memcpy(buff + sizeof(client_id), &uid, sizeof(uid));
+
+    return PSA_SUCCESS;
+}
+#endif
