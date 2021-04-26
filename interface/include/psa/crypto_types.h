5c23d1e17fbdb6bafa2c4c6dd01c16f48d9785ef
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2021-04-26 08:42:59 +0200

    [nrf noup] add missing items in TF-M interface headers
    -crypto_types.h: Added definition of mbedtls_svc_key_id_t needed
     for NS build TLS/DTLS APIs
    
    -crypto_values.h: Added lots of functions for mbedtls_svc_key_id_t
     needed for NS build TLS/DTLS APIs
    
    Add missing function for TLS/DTLS usage in crypto_extra.h. This needs
    to go upstream, but most likely containing more functions.
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
    (cherry picked from commit 8390b3c3ef444d25eddd1c6a1230d051890b2bea)

--- interface/include/psa/crypto_types.h
+++ interface/include/psa/crypto_types.h
@@ -207,6 +207,23 @@ typedef uint32_t psa_key_id_t;
 
 /**@}*/
 
+#if !defined(MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER)
+typedef psa_key_id_t mbedtls_svc_key_id_t;
+
+#else /* MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER */
+/* Implementation-specific: The Mbed Cryptography library can be built as
+ * part of a multi-client service that exposes the PSA Cryptograpy API in each
+ * client and encodes the client identity in the key identifier argument of
+ * functions such as psa_open_key().
+ */
+typedef struct
+{
+    psa_key_id_t key_id;
+    mbedtls_key_owner_id_t owner;
+} mbedtls_svc_key_id_t;
+
+#endif /* !MBEDTLS_PSA_CRYPTO_KEY_ID_ENCODES_OWNER */
+
 /** \defgroup policy Key policies
  * @{
  */
