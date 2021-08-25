d1c70fbee87e2e9cd917f68bf771dc7618cee180
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2021-08-25 10:59:55 +0200

    Revert "[nrf fromlist] trusted-firmware-m: Enable ChachaPoly support in the configuration"
    This reverts commit d747c3ce3c457461188c4c92bcf8a6acb3118813.
    
    -We took the decision to enable the configuration
     in the sdk-nrf repo
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- trusted-firmware-m/lib/ext/mbedcrypto/mbedcrypto_config/tfm_mbedcrypto_config_default.h
+++ trusted-firmware-m/lib/ext/mbedcrypto/mbedcrypto_config/tfm_mbedcrypto_config_default.h
@@ -1205,7 +1205,7 @@
  *
  * Module:  library/chacha20.c
  */
-#define MBEDTLS_CHACHA20_C
+//#define MBEDTLS_CHACHA20_C
 
 /**
  * \def MBEDTLS_CHACHAPOLY_C
@@ -1216,7 +1216,7 @@
  *
  * This module requires: MBEDTLS_CHACHA20_C, MBEDTLS_POLY1305_C
  */
-#define MBEDTLS_CHACHAPOLY_C
+//#define MBEDTLS_CHACHAPOLY_C
 
 /**
  * \def MBEDTLS_CIPHER_C
@@ -1700,7 +1700,7 @@
  * Module:  library/poly1305.c
  * Caller:  library/chachapoly.c
  */
-#define MBEDTLS_POLY1305_C
+//#define MBEDTLS_POLY1305_C
 
 /**
  * \def MBEDTLS_PSA_CRYPTO_C
