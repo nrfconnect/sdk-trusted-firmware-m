6be90b812d6f1f4249237fe18f065acc8738a06f
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-05-27 13:14:31 +0200

    [nrf toup] platform: nordic_nrf: Add option to initialize hardware to reset on boot
    Add an option to the nordic platforms to initialize the hardware state
    to the its reset values when the TF-M firmware is booting.
    The option is useful when the TF-M firmware image is chain-loaded, for
    example, a bootloader, and we need to guarantee that the internal
    states of the architecture core blocks are restored to the reset values.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    (cherry picked from commit 9f3911b8d3c0116d208e17789de4e9fd4b9ff0be)
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

---
+++ platform/ext/target/nordic_nrf/common/core/hw_init.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef HW_INIT_H__
+#define HW_INIT_H__
+
+#define _SCS_BASE_ADDR 0xE000E000
+#define _SCS_MPU_CTRL (_SCS_BASE_ADDR + 0xd94)
+
+#endif /* HW_INIT_H__*/
