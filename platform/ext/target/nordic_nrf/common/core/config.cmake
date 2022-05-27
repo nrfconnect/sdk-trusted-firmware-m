9f3911b8d3c0116d208e17789de4e9fd4b9ff0be
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-05-27 13:14:31 +0200

    [nrf toup] platform: nordic_nrf: Add option to initialize hardware to reset on boot
    Add an option to the nordic platforms to initialize the hardware state
    to the its reset values when the TF-M firmware is booting.
    The option is useful when the TF-M firmware image is chain-loaded, for
    example, a bootloader, and we need to guarantee that the internal
    states of the architecture core blocks are restored to the reset values.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- platform/ext/target/nordic_nrf/common/core/config.cmake
+++ platform/ext/target/nordic_nrf/common/core/config.cmake
@@ -8,3 +8,5 @@
 set(HAL_NORDIC_PATH "DOWNLOAD" CACHE PATH "Path to the Nordic HAL (or DOWNLOAD to fetch automatically)")
 set(HAL_NORDIC_VERSION "004b62c0b74c62bd8489e0a702a498899e0463d1" CACHE STRING "Version of the Nordic HAL to download") # nrfx 2.6.0
 set(HAL_NORDIC_REMOTE "https://github.com/zephyrproject-rtos/hal_nordic" CACHE STRING "Remote of the Nordic HAL to download")
+
+set(NRF_HW_INIT_RESET_ON_BOOT CACHE BOOL OFF "Initialize internal architecture state at boot")
