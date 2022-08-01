ffb74106f1d159c8f2469a0ebfa65c7c62045af5
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-08-01 14:27:59 +0200

    [nrf fromlist] platform: nordic_nrf: Separate secondary partition from BL2 config
    Separate the nordic_nrf platform support for including the secondary
    image partition from the BL2 configuration.
    This is required in order to support two use-cases.
    - Using a different bootloader than BL2.
    - Supporting the secondary partition in external flash (the SPU should
    not configure this partition as non-secure in that case).
    
    Upstream PR:
    https://review.trustedfirmware.org/c/TF-M/trusted-firmware-m/+/16414
    
    Change-Id: Icfaa5df972d7c2776b326f4f30e6ea5f3487d35e
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    (cherry picked from commit 48b6f0742160abf679ce27dc28ba1b6cdd4b8c96)

--- platform/ext/target/nordic_nrf/common/nrf9160/config.cmake
+++ platform/ext/target/nordic_nrf/common/nrf9160/config.cmake
@@ -12,5 +12,6 @@ set(SECURE_UART1                        ON         CACHE BOOL      "Enable secur
 set(PSA_API_TEST_TARGET                 "nrf9160"  CACHE STRING    "PSA API test target")
 set(ITS_NUM_ASSETS                      "5"        CACHE STRING    "The maximum number of assets to be stored in the Internal Trusted Storage area")
 set(NRF_NS_STORAGE                      OFF        CACHE BOOL      "Enable non-secure storage partition")
+set(NRF_NS_SECONDARY                    ${BL2}     CACHE BOOL      "Enable non-secure secondary partition")
 set(TFM_ITS_ENC_NONCE_LENGTH            "12"       CACHE STRING    "The size of the nonce used in ITS encryption in bytes")
 set(TFM_ITS_AUTH_TAG_LENGTH             "16"       CACHE STRING    "The size of the tag used in ITS encryption in bytes")
