147e29e483b2589f0dd3cf901dd9b4c4e91c31ea
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-10-06 12:50:25 +0200

    [nrf toup] secure_fw: Add option to log output on a shared UART instance.
    Add an option to send the log output from the secure firmware on a
    UART instance that would be shared with the non-secure application.
    
    This option is added where the number of UART instances is limited
    and the application only cares about the receiving the TF-M log
    on fatal errors.
    
    To allow this option to be enabled the log is disabled in the boot
    process before the non-secure application is started.
    It is enabled again when an unrecoverable exception has occurred in
    the secure firmware.
    
    NCSDK-18595
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- platform/ext/common/exception_info.c
+++ platform/ext/common/exception_info.c
@@ -9,6 +9,7 @@
 #include "exception_info.h"
 #include "tfm_spm_log.h"
 #include "tfm_core_utils.h"
+#include "uart_stdout.h"
 
 struct exception_info_t {
     uint32_t EXC_RETURN;        /* EXC_RETURN value in LR. */
@@ -175,6 +176,10 @@ static void dump_exception_info_t(bool stack_error,
 static void dump_error(uint32_t error_type)
 {
     bool stack_error = false;
+
+#if defined(CONFIG_TFM_LOG_SHARE_UART)
+    stdio_init();
+#endif
 
     SPMLOG_ERRMSG("FATAL ERROR: ");
     switch (error_type) {
