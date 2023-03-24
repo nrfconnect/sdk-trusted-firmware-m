ffa268f5a71471c291fdaaac1eface16955b1616
Markus Swarowsky <markus.swarowsky@nordicsemi.no>
2023-03-24 15:06:16 +0100

    [nrf toup]: ITS: Fix header includes
    fixup! [nrf toup]: ITS: Add support for encrypted ITS
    
    The C++ style header was used, changed to C sytle.
    
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>
    Change-Id: Ic8fd152a4447d91f594cd85401e44236ccb3d1d4
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- platform/ext/target/nordic_nrf/common/core/tfm_hal_its_encryption.c
+++ platform/ext/target/nordic_nrf/common/core/tfm_hal_its_encryption.c
@@ -18,7 +18,7 @@
 #include "platform/include/tfm_hal_its.h"
 #include "psa/crypto.h"
 #include <string.h>
-#include <stdint>
+#include <stdint.h>
 
 /* The cc3xx driver is used directly in this file as the PSA APIs depends on
  * ITS, so using PSA APIs would result in circular dependencies
