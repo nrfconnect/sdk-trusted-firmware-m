20df0307ff41b7f23da808a1fa42f4f3906c6406
Joakim Andersson <joakim.andersson@nordicsemi.no>
2023-07-03 12:13:10 +0200

    [nrf noup] Include PSA config file in TF-M PSA interface header
    Include the PSA config file in TF_M PSA interface header.
    
    This is a noup commit because it is and adapted change based on upstream
    commit that contains other changes that would cause merge conflicts.
    An additional PR was made to upstream to improve the way the config
    header is included.
    
    Upstream commit:
    34a0ffd0d14bf98c8dc517e3982c4ed8cec9f56f
    Upstream PR:
    https://review.trustedfirmware.org/c/TF-M/trusted-firmware-m/+/21759
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    (cherry picked from commit 86fdfb9b5fa41172dced90b5262ab11281823430)
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>
    Change-Id: I048f4174486a911bd1451de5a244446601608d29
    Signed-off-by: Markus Swarowsky <markus.swarowsky@nordicsemi.no>

--- interface/include/psa/crypto.h
+++ interface/include/psa/crypto.h
@@ -18,6 +18,17 @@
 #include "crypto_platform.h"
 #endif
 
+/* In Mbed TLS, we would query the current config through inclusion of
+ * of mbedtls/build_info.h, but in TF-M, we don't rely on build_info.h
+ * hence we just include the current configuration if it has been passed
+ * through command line. These config defines are required in crypto_sizes.h
+ * to compute macros that define sizes which depend on algorithms supported
+ * by the implementation
+ */
+#if defined(MBEDTLS_PSA_CRYPTO_CONFIG_FILE)
+#include MBEDTLS_PSA_CRYPTO_CONFIG_FILE
+#endif /* MBEDTLS_PSA_CRYPTO_CONFIG_FILE */
+
 #include <stddef.h>
 
 #ifdef __DOXYGEN_ONLY__
