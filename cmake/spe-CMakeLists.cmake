e6376051622272261f8a913a363c695d467777bd
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-05 15:52:15 +0200

    [nrf noup] tf-m: Enable support for external PSA core
    -This commit adds support for externally built PSA core in TF-M
     by checking for the CMake variable (cached) PSA_CRYPTO_EXTERNAL_CORE.
     By setting this define, then a platform-target file called
     external_core.cmake as well as external_core_install.cmake is called
     to allow for the following:
     - Early include of necessary replacement include folders
     - Support for using generated configuration files for TF-M build
    -This commit also tries to make psa_crypto_config and
     psa_crypto_library_config linked in first to ensure that certain
     folders are included as early as possible in the build
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- cmake/spe-CMakeLists.cmake
+++ cmake/spe-CMakeLists.cmake
@@ -34,6 +34,15 @@ target_sources(tfm_api_ns
 )
 
 # Include interface headers exported by TF-M
+if(PSA_CRYPTO_EXTERNAL_CORE)
+    include(${TARGET_PLATFORM_PATH}/../external_core.cmake)
+else()
+    target_include_directories(tfm_api_ns
+        PUBLIC
+            ${INTERFACE_INC_DIR}
+    )
+endif()
+
 target_include_directories(tfm_api_ns
     PUBLIC
         ${INTERFACE_INC_DIR}
