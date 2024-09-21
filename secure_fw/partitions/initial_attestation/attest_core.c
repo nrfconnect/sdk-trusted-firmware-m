f5303ad9e528fdd7322610f586afb815c81ac634
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2024-09-21 14:57:20 +0200

    [nrf noup] attestation: Prevent errors of multiple defs of ARRAY_LENGTH
    -The macro ARRAY_LENGTH is defined without checking if there is already
     a definition. This commit can be reverted once the proposed fix
     is handled upstream
    -This fixes ARRAY_LENGTH in s_io_sorage_tests.c
    
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- secure_fw/partitions/initial_attestation/attest_core.c
+++ secure_fw/partitions/initial_attestation/attest_core.c
@@ -23,7 +23,9 @@
 #include "tfm_crypto_defs.h"
 #include "tfm_sp_log.h"
 
+#ifndef ARRAY_LENGTH
 #define ARRAY_LENGTH(array) (sizeof(array) / sizeof(*(array)))
+#endif
 
 /*!
  * \brief Static function to map return values between \ref psa_attest_err_t
