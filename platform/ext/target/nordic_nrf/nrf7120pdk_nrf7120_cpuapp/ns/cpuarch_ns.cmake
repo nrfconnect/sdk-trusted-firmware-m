3acae6a71406c357b030a1bf769782483ac6e94a
Travis Lam <travis.lam@nordicsemi.no>
2025-09-10 17:41:44 +0200

    platform: nrf7120: Add support for nrf7120
    Add initial tfm support for nrf7120
    This is based on nrf54l series soc, should have similar settings
    Added startup_nrf71 and target_cfg_71 to support this
    
    Change-Id: I83d9b971c4e46fc300092fd0badd30e2023639cc
    Signed-off-by: Travis Lam <travis.lam@nordicsemi.no>
    (cherry picked from commit ae97268b372236bb1dc035955b2dd61aa1b03626)

---
+++ platform/ext/target/nordic_nrf/nrf7120pdk_nrf7120_cpuapp/ns/cpuarch_ns.cmake
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2024, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
+
+set(PLATFORM_DIR  ${CMAKE_CURRENT_LIST_DIR})
+set(PLATFORM_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+add_compile_definitions(NRF_CONFIG_CPU_FREQ_MHZ=256)
+
+include(${CMAKE_CURRENT_LIST_DIR}/common/nrf7120/cpuarch.cmake)
