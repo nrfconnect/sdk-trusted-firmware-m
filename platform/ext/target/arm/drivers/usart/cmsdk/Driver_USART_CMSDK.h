370bbddc125a0791e3e9fc414bf393fec993e7f8
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2025-12-12 11:20:14 +0200

    Revert "[zep fromtree] Platform: Fix return-type compiler warnings"
    This reverts commit 5a6a3cdf4565a331dd0e4e525c5860c94463448b.

--- platform/ext/target/arm/drivers/usart/cmsdk/Driver_USART_CMSDK.h
+++ platform/ext/target/arm/drivers/usart/cmsdk/Driver_USART_CMSDK.h
@@ -84,7 +84,6 @@ static inline int32_t ARM_USARTx_PowerControl(UARTx_Resources *uart_dev,
      *           compiler to check that all the enumeration values are
      *           covered in the switch.*/
     }
-    return ARM_DRIVER_ERROR;
 }
 
 static inline int32_t ARM_USARTx_Send(UARTx_Resources *uart_dev,
