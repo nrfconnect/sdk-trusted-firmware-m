2e17d543b09bfaddcafa648ab4aae732c6d4fca1
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2026-05-11 13:32:57 +0300

    [nrf noup] don't install Oberon header files
    nrf-squash! [nrf noup] tf-m: Enable support for external PSA core
    
    Was useless. Include directories are here for this.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -83,7 +83,6 @@ endif()
 
 if (TFM_PARTITION_CRYPTO)
         if(PSA_CRYPTO_EXTERNAL_CORE)
-                include(${TFM_BOARDS_NRF_DIR}/external_core_install.cmake)
                 install(FILES       ${INTERFACE_INC_DIR}/tfm_crypto_defs.h
                         DESTINATION ${INSTALL_INTERFACE_INC_DIR})
         else()
